dc2bad7dd68d768194fa285d618d2c8bab0adde9
Evseev Anton <soki1997@mail.ru>
2023-03-30 21:03:18 +0500

    fix: favicon.ico

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
-import {createRoot} from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import AppWithRedux from './AppWithRedux';
-import {store} from './state/store';
+import App from './app/App';
+import {store} from './app/store';
 import {Provider} from 'react-redux';
 
-const container = document.getElementById('root') as HTMLElement
-const root = createRoot(container);
-root.render(
+ReactDOM.render(
     <Provider store={store}>
-        <AppWithRedux/>
-    </Provider>
-);
+        <App/>
+    </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
